f8b70680008296dbddc857c13da4e545f3a252c4
Nika <v.nika.olesova@gmail.com>
2013-03-10 17:47:44 +0100

    Initial commit

---
+++ rails_projects/firt_app/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FirtApp::Application.config.secret_token = '95cfadca74729d6bc2f14ad418049f3f9a2cda12aa9f28a5fa2e5353b39a03c5faf51b4db62ec13e00ec890ee47bc76bc912c3512670c2c36fb38ed711681211'
